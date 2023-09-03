ead4b17d98ffdedfc8aac7354a5b409c5c638745
larry-at-737410041159 <elandel35@gmail.com>
2023-09-02 22:34:03 -0600

    commented out restTemplate tests because they only work when app running

--- src/test/java/com/oreillys/pos/invoice/RestApiCallTests.java
+++ src/test/java/com/oreillys/pos/invoice/RestApiCallTests.java
@@ -7,16 +7,16 @@ import org.springframework.web.client.RestTemplate;
 
 import java.util.Map;
 
-public class RestApiCallTests {
+//public class RestApiCallTests {
 
-    @Test
-    public void testMapReturnedFromApiCall() {
-        ParameterizedTypeReference<Map<Long,String>> responseType =
-                new ParameterizedTypeReference<>() {};
-        RestTemplate restTemplate = new RestTemplate();
-        String resource = "http://localhost:8080/api/invoice/tendertype/customer/2";
-        RequestEntity<Void> requestEntity = RequestEntity.get(resource).build();
-        Map<Long, String> theMap = restTemplate.exchange(requestEntity, responseType).getBody();
-        theMap.entrySet().stream().forEach(e -> System.out.println(e.getKey() + " - " + e.getValue()));
-    }
-}
+//    @Test
+//    public void testMapReturnedFromApiCall() {
+//        ParameterizedTypeReference<Map<Long,String>> responseType =
+//                new ParameterizedTypeReference<>() {};
+//        RestTemplate restTemplate = new RestTemplate();
+//        String resource = "http://localhost:8080/api/invoice/tendertype/customer/2";
+//        RequestEntity<Void> requestEntity = RequestEntity.get(resource).build();
+//        Map<Long, String> theMap = restTemplate.exchange(requestEntity, responseType).getBody();
+//        theMap.entrySet().stream().forEach(e -> System.out.println(e.getKey() + " - " + e.getValue()));
+//    }
+//}
